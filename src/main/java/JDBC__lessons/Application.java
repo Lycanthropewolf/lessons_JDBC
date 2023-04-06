cd3bb2683e1bd8f27f708ba0fc3198ba1d265f7b
Антон <antonolegovich2017@icloud.com>
2023-04-06 16:36:05 +0600

    dz

--- src/main/java/JDBC__lessons/Application.java
+++ src/main/java/JDBC__lessons/Application.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Application {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args)  throws SQLException {
         try (Connection connection = getConnection()) {
 
         }
