f8adfd36308f5e4a27dba0561301aa5e0ac7fb88
Антон <antonolegovich2017@icloud.com>
2023-04-06 17:14:22 +0600

    dz

--- src/main/java/JDBC__lessons/Application.java
+++ src/main/java/JDBC__lessons/Application.java
@@ -1,19 +1,29 @@
 package JDBC__lessons;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Application {
-    public static void main(String[] args)  throws SQLException {
-        try (Connection connection = getConnection()) {
+    public static void main(String[] args) throws SQLException {
+        String query = "SELECT * FROM employee WHERE id= ?";
 
+        try (Connection connection = getConnection()){
+             PreparedStatement statement=connection.prepareStatement(query);
+             statement.setInt(1,1);
+             ResultSet resultSet = statement.executeQuery();
+             while (resultSet.next()) {
+                int id = resultSet.getInt(1);
+                System.out.println("id = "+id);
+                String name = resultSet.getString(2);
+                System.out.println("name = "+name);
+            }
         }
 
     }
 
+
+
     private static Connection getConnection() throws SQLException {
-        final String url = "jdbc:postgresql://localhost:5433/postgres";
+        final String url = "jdbc:postgresql://localhost:5433/mybase";
         final String user = "postgres";
         final String passvord = "cg7pdrpp";
         return DriverManager.getConnection(url, user, passvord);
