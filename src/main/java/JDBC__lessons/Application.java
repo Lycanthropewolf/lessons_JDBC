2dbe6ce782698779d831a7e64a260d4a0324d453
Антон <antonolegovich2017@icloud.com>
2023-04-07 20:02:26 +0600

    dz

--- src/main/java/JDBC__lessons/Application.java
+++ src/main/java/JDBC__lessons/Application.java
@@ -1,21 +1,45 @@
 package JDBC__lessons;
 
+import JDBC__lessons.Model.City;
+import JDBC__lessons.Model.Employee;
+import JDBC__lessons.Service.EmployeeDao;
+import JDBC__lessons.Service.EmployeeDaoImpl;
+
 import java.sql.*;
 
 public class Application {
     public static void main(String[] args) throws SQLException {
+        EmployeeDao employeeDao = new EmployeeDaoImpl();
+        City city=new City(5,"Moscow");
+        Employee employee = new Employee(12,"ffef", "fewfw", "wfwf", 52, city);
+
+        employeeDao.add(employee);
+
+
+
+
+
+
+
+
+
+
+
+
         final String url = "jdbc:postgresql://localhost:5433/mybase";
         final String user = "postgres";
         final String passvord = "cg7pdrpp";
+
+
         try (final Connection connection = DriverManager.getConnection(url, user, passvord);
              PreparedStatement statement = connection.prepareStatement("" + "SELECT * FROM employee WHERE id=(?)")) {
-            statement.setInt(1, 4);
+            statement.setInt(1, 11);
             final ResultSet resultSet = statement.executeQuery();
-            if (resultSet.next()){
-                String name="Name: "+resultSet.getString("first_name");
-                String surname="Surname: "+resultSet.getString("last_name");
-                String gender="Gender: "+resultSet.getString("gender");
-                int age=resultSet.getInt(5);
+            if (resultSet.next()) {
+                String name = "Name: " + resultSet.getString("first_name");
+                String surname = "Surname: " + resultSet.getString("last_name");
+                String gender = "Gender: " + resultSet.getString("gender");
+                int age = resultSet.getInt(5);
 
                 System.out.println(name);
                 System.out.println(surname);
---
+++ src/main/java/JDBC__lessons/Service/EmployeeDaoImpl.java
@@ -0,0 +1,109 @@
+package JDBC__lessons.Service;
+
+import JDBC__lessons.Model.City;
+import JDBC__lessons.Model.Employee;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeDaoImpl implements EmployeeDao {
+    final String url = "jdbc:postgresql://localhost:5433/mybase";
+    final String user = "postgres";
+    final String passvord = "cg7pdrpp";
+
+    @Override
+    public void add(Employee employee) throws SQLException {
+        try (final Connection connection = DriverManager.getConnection(url, user, passvord);
+             PreparedStatement statement = connection.prepareStatement(
+                     "INSERT INTO employee (id, first_name, last_name, gender, age, city_id) VALUES ((?),(?),(?),(?),(?),(?))");) {
+            statement.setInt(1, employee.getId());
+            statement.setString(2, employee.getFirst_name());
+            statement.setString(3, employee.getLast_name());
+            statement.setString(4, employee.getGender());
+            statement.setInt(5, employee.getAge());
+            statement.setInt(6, employee.getCity().getCity_id());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public Employee getById(int id) {
+        Employee employee = new Employee();
+
+        try (final Connection connection = DriverManager.getConnection(url, user, passvord);
+             PreparedStatement statement = connection.prepareStatement("SELECT * FROM employee INNER JOIN city "
+                     + "ON employee.city_id = city.city_id WHERE id=(?)")) {
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                employee.setId(resultSet.getInt(1));
+                employee.setFirst_name(resultSet.getString(2));
+                employee.setLast_name(resultSet.getString(3));
+                employee.setGender(resultSet.getString(4));
+                employee.setAge(resultSet.getInt(5));
+                employee.setCity(new City((resultSet.getInt("city_id")), resultSet.getString("city_name")));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return employee;
+    }
+
+    @Override
+    public List<Employee> getAllEmployee() {
+        List<Employee> employees = new ArrayList<>();
+        try (final Connection connection = DriverManager.getConnection(url, user, passvord);
+             PreparedStatement statement = connection.prepareStatement("SELECT * FROM employee INNER JOIN city "
+                     + "ON employee.city_id = city.city_id")) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                int id = Integer.parseInt(resultSet.getString(1));
+                String first_name = resultSet.getString(2);
+                String last_name = resultSet.getString(3);
+                String gender = resultSet.getString(4);
+                int age = Integer.parseInt(resultSet.getString(5));
+                City city = new City(resultSet.getInt("city_id"), resultSet.getString("city_name"));
+                employees.add(new Employee(id, first_name, last_name, gender, age, city));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        return employees;
+    }
+
+    @Override
+    public void updateEmployee(int id, Employee employee) {
+        try (final Connection connection = DriverManager.getConnection(url, user, passvord);
+             PreparedStatement statement = connection.prepareStatement(
+                     "UPDATE employee SET first_name=(?), last_name = (?), gender=(?), age=(?), city_id =(?) WHERE id=(?)")) {
+            statement.setString(1, employee.getFirst_name());
+            statement.setString(2, employee.getLast_name());
+            statement.setString(3, employee.getGender());
+            statement.setInt(4, employee.getAge());
+            statement.setInt(5, employee.getCity().getCity_id());
+            statement.setInt(6, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+        try (final Connection connection = DriverManager.getConnection(url, user, passvord);
+             PreparedStatement statement = connection.prepareStatement(
+                     "DELETE FROM employee WHERE id=(?)")) {
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
