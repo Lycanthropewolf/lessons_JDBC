ade75073c4e5806fff426f14afd16066c30059a5
Антон <antonolegovich2017@icloud.com>
2023-04-17 20:03:59 +0600

    dz

--- src/main/java/Hibernate/Application.java
+++ src/main/java/Hibernate/Application.java
@@ -4,17 +4,18 @@ import Hibernate.Model.Employee;
 import Hibernate.Service.EmployeeDao;
 import Hibernate.Service.EmployeeDaoImpl;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         Employee employee = new Employee(134, "Denis", "Sergeev", "male", 56, 3);
         EmployeeDao employeeDao = new EmployeeDaoImpl();
 
-        System.out.println(employeeDao.getById(1));
-        System.out.println(employeeDao.add(employee));
-        System.out.println(employeeDao.getAllEmployee());
-        employeeDao.updateEmployee(employee,100);
-        System.out.println(employeeDao.getById(100));
-        employeeDao.deleteEmployee(employee);
+        System.out.println(employeeDao.getById(2));
+        List<Employee> list=employeeDao.getAllEmployee();
+        for (Employee employee1: list){
+            System.out.println(employee1);
+        }
     }
 
 
